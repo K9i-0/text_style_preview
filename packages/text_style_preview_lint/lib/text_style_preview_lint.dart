2800deadf2fb7e8c26b1692cb611c89f402a39cb
K9i-0 <geukfyger@gmail.com>
2023-04-12 19:12:27 +0900

    init text_style_preview_lint

---
+++ packages/text_style_preview_lint/lib/text_style_preview_lint.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library text_style_preview_lint;
+
+export 'src/text_style_preview_lint_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
